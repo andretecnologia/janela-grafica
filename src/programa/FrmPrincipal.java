fc52aa5d4f870f5409078cd28806cdf988bf88dd
andretecnologia <andre@andretecnologia.com.br>
2021-01-16 13:04:50 -0300

    Iniciando o projeto com janela grafica

---
+++ src/programa/FrmPrincipal.java
@@ -0,0 +1,10 @@
+package programa;
+
+public class FrmPrincipal  extends javax.swing.JFrame {
+	
+	private static final long serialVersionUID = 1L;
+
+	public FrmPrincipal() {
+		
+	}
+}
---
+++ src/programa/Programa.java
@@ -0,0 +1,13 @@
+package programa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		java.awt.EventQueue.invokeLater( new Runnable() {	
+			public void run() {
+				
+				
+			}	
+		});
+	}
+}
